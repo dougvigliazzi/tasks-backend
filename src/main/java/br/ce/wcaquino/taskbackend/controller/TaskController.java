8fd6f3797c9d5cc0d7cd41e8460c83dce5920d82
Douglas <dougvigliazzi@gmail.com>
2021-04-22 17:02:35 +0300

    remove Remove task

--- src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
@@ -42,6 +42,7 @@ public class TaskController {
 		Task saved = todoRepo.save(todo);
 		return new ResponseEntity<Task>(saved, HttpStatus.CREATED);
 	}
+	
 	@DeleteMapping(value = "/{id}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
